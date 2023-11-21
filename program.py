5d3265983b5135878f36c4c70cfc9aeec2d777d4
HackerMan888 <144242698+HackerMan888@users.noreply.github.com>
2023-11-21 23:26:22 +0000

    complete assignment

--- program.py
+++ program.py
@@ -7,7 +7,15 @@
 # End at 32
 
 result = ""
-for myNumber in range(32):
-  result = str(result) + str(myNumber) + "\n"
+# loop from 1 to 32 so the end range needs to be 1 more
+for myNumber in range(1, 33):
+  if myNumber % 15 == 0:
+    result = str(result) + str('FizzBuzz') + "\n"
+  elif myNumber % 5 == 0:
+    result = str(result) + str('Buzz') + "\n"
+  elif myNumber % 3 == 0:
+    result = str(result) + str('Fizz') + "\n"
+  else:
+    result = str(result) + str(myNumber) + "\n"
 
 print(result)
